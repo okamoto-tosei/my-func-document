fb5804228945859e64f8984def855bd33971d751
okamoto tosei <tosei393939@gmail.com>
2021-03-06 18:58:35 +0900

    test

---
+++ index.js
@@ -0,0 +1,19 @@
+// 正規表現
+
+//郵便番号３桁ー４桁の正規表現（ハイフンは有り無しどちらでもいい）
+const zipNumTestFunc = (zip1, zip2) => {
+  //  /^　開始
+  // \d 前になんの文字もないことを示す
+  // $/g 終わりの呪いgはオプション
+  const zip_pattern = /^\d{3}-?\d{4}$/g;
+
+  if(zip_pattern.test(String(zip1) + String(zip2))) {
+    return "正しく入力されました。"
+  } else  {
+    return "正しい文字を入力してください"
+  }
+}
+
+// 動作確認用
+const zip = zipNumTestFunc(555,2330)
+console.log(zip)
